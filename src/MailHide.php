d08affd40325549f0424bc645179aa4ff512ae5d
Andi Rückauer <arueckauer@gmail.com>
2021-10-01 16:48:57 +0200

    Remove file headers
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- src/MailHide.php
+++ src/MailHide.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-recaptcha for the canonical source repository
- * @copyright https://github.com/laminas/laminas-recaptcha/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-recaptcha/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\ReCaptcha;
 
 use Exception;
