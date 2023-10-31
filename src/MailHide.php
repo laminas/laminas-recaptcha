283fc16dcd766abc97783ee0e53569165ce701b9
Saurabh Kumar <glo17680@adobe.com>
2023-10-31 18:08:00 +0530

    Baselined psalm errors

--- src/MailHide.php
+++ src/MailHide.php
@@ -279,10 +279,6 @@ class MailHide extends ReCaptcha
             $emailParts[0] = substr($emailParts[0], 0, 4);
         }
 
-        if(!isset($emailParts[1]) {
-            $emailParts[1] = "";
-        }
-
         $this->emailLocalPart  = $emailParts[0];
         $this->emailDomainPart = $emailParts[1];
 
